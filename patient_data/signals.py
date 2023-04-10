90c5a19f248e64b6a87661a3bcb88ec84d04153a
Kundan Singh Chufal <chufal@Kundans-MacBook-Pro.local>
2023-04-10 22:02:03 +0530

    signals.py updated to update simulation as per the assessment module

--- patient_data/signals.py
+++ patient_data/signals.py
@@ -1,16 +1,22 @@
 from django.db.models.signals import post_save
 
-from patient_data.models import S7Assessment, Simulation, S3CarePlan, S8FUP, S4RT, RTStatus
+from patient_data.models import S7Assessment, Simulation, S3CarePlan, S8FUP, S4RT, RTStatus, SimStatus
 
 
 def simulationUpdate(sender, instance, created, **kwargs):
     patient_sim = Simulation.objects.get(simid=instance.s4_id.simid.simid)
     patient_sim.donefr = instance.fxdone
     patient_sim.as_date = instance.as_date
+
     if instance.txstatus == "Last Day":
         patient_sim.finalstatus = 1  # treatment completed 1
+        if patient_sim.initialstatus.status.startswith('Ready') or patient_sim.initialstatus.status.startswith('Started'):
+            patient_sim.initialstatus = SimStatus.objects.get(code="Completed(" + patient_sim.initialstatus.status.split('(')[1])
+            patient_sim.actualcompletiondate = instance.as_date
     else:
         patient_sim.finalstatus = 0  # treatment ongoing 0
+        if patient_sim.initialstatus.status.startswith('Ready'):
+            patient_sim.initialstatus = SimStatus.objects.get(code="Started (" + patient_sim.initialstatus.split('(')[1])
     patient_sim.save()
 
 
@@ -40,7 +46,6 @@ def radiotherapyUpdate(sender, instance, created, **kwargs):
     else:
         rt_status = RTStatus.objects.get(pk="0")
         patient_rt.rtstatus = rt_status
-
     patient_rt.save()
 
 
